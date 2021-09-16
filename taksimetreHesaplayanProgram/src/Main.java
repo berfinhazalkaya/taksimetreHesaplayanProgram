1b147c63c88487774a30f9bcaa0a67deb6ffd1c9
Berfin Hazal Kaya <berfinhazalkaya@gmail.com>
2021-09-17 01:43:53 +0300

    Java Patikasi Taksimetre Hesaplayan Program
    Java Patikasi Taksimetre Hesaplayan Program

---
+++ taksimetreHesaplayanProgram/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int km;
+        double perKM = 2.20;
+        double total;
+        double startPrice=10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM Cinsinden Yazınız: ");
+        km = input.nextInt();
+
+        total = (km * perKM);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam Tutar : " + total + "TL");
+    }
+
+}
